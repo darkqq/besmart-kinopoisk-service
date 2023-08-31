8eeb0ada4f35f3bacd6811d4f58cfc32be51988e
Makseke <96065260+Makseke@users.noreply.github.com>
2023-08-31 15:52:24 +0300

    Deleted Exemple package

--- src/test/java/com/example/besmartkinopoiskservice/BesmartKinopoiskServiceApplicationTests.java
+++ src/test/java/com/example/besmartkinopoiskservice/BesmartKinopoiskServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.besmartkinopoiskservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BesmartKinopoiskServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BesmartKinopoiskServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
