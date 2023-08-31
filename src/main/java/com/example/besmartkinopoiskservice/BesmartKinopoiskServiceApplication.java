9ce84aec7d6b26477321e19773af383a42556c81
Makseke <96065260+Makseke@users.noreply.github.com>
2023-08-31 14:20:22 +0300

    Added Spring

---
+++ src/main/java/com/example/besmartkinopoiskservice/BesmartKinopoiskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.besmartkinopoiskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesmartKinopoiskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesmartKinopoiskServiceApplication.class, args);
+	}
+
+}
